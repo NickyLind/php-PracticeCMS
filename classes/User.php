43e03c82b6e8dde4a300a22bc797ce45109d2481
Nick Lindau <nicholaithegreat@gmail.com>
2022-11-11 18:14:36 -0800

    move authentication into an Auth class

--- classes/User.php
+++ classes/User.php
@@ -49,7 +49,7 @@ class User
     $stmt->execute();
 
     if ($user = $stmt->fetch()) {
-      return $user->password == $password;
+      return password_verify($password, $user->password);
     }
   }
 }
