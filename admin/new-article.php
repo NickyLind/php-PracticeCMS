a1c0ec816290ba5ac31ff41ab71147a4b4a25a9a
Nick Lindau <nicholaithegreat@gmail.com>
2023-01-26 15:23:03 -0800

    Add categories when inserting a new Article record

--- admin/new-article.php
+++ admin/new-article.php
@@ -5,17 +5,23 @@ require '../includes/init.php';
 Auth::requireLogin();
 
 $article = new Article();
+$category_ids = [];
+$conn = require '../includes/db.php';
+$categories = Category::getAll($conn);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
-  $conn = require '../includes/db.php';
 
   $article->title = $_POST['title'];
   $article->content = $_POST['content'];
   $article->published_at = $_POST['published_at'];
 
+  $category_ids = $_POST['category'] ?? [];
+
   if ($article->create($conn)) {
-      Url::redirect("/CMS/admin/article.php?id={$article->id}");
+    $article->setCategories($conn, $category_ids);
+
+    Url::redirect("/CMS/admin/article.php?id={$article->id}");
   }
 }
 
