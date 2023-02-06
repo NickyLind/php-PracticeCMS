19db427ac52f5b92b0e847c9859c3fb603b06034
Nick Lindau <nicholaithegreat@gmail.com>
2023-02-05 18:49:46 -0800

    add an ErrorException example

--- error-example.php
+++ error-example.php
@@ -6,12 +6,19 @@
 // }
 // set_error_handler('errorHandler');
 
+function errorHandler($level, $message, $file, $line)
+{
+  throw new ErrorException($message, 0, $level, $file, $line);
+}
+set_error_handler('errorHandler');
+
 function exceptionHandler($exception)
 {
   echo 'Caught exception: ', $exception->getMessage();
 }
 set_exception_handler('exceptionHandler');
 
-$i = 1 / 0;
+// $i = 1 / 0;
+$datetime = new DateTime('invalid');
 
 ?>
