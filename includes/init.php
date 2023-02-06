57d4b7ffc0fd6e9de9984a1fccfb976b537e1122
Nick Lindau <nicholaithegreat@gmail.com>
2023-02-05 19:01:52 -0800

    add error and exception handlers to site along with defining a global variable for showing/hiding debug info

--- includes/init.php
+++ includes/init.php
@@ -13,3 +13,33 @@ spl_autoload_register(function($class) {
 session_start();
 
 require dirname(__DIR__) . '/config.php';
+
+function errorHandler($level, $message, $file, $line)
+{
+  throw new ErrorException($message, 0, $level, $file, $line);
+}
+
+function exceptionHandler($exception)
+{
+  if(SHOW_ERROR_DETAIL) {
+    echo sprintf(
+      '<h1>An Error Occurred</h1>
+      <p>Uncaught exception: %1$s</p>
+      <p>%2$s</p>
+      <p>Stack Trace: <pre>%3$s</pre></p>
+      <p>In file %4$s on line %5$s</p>',
+      get_class($exception),
+      $exception->getMessage(),
+      $exception->getTraceAsString(),
+      $exception->getFile(),
+      $exception->getLine()
+    );
+    exit();
+  } else {
+    echo '<h1>An error occurred</h1>';
+    echo '<p>Please try again later.</p>';
+  }
+}
+
+set_error_handler('errorHandler');
+set_exception_handler('exceptionHandler');
